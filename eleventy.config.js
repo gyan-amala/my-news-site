1b2b2fff93172d03c544d2eac6171beb08b66f5a
gyan-amala <contactgyanamala@gmail.com>
2026-07-05 23:55:00 +0530

    Add passthrough copy for style.css to config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,8 @@
 module.exports = function(eleventyConfig) {
   
+  // ADD THIS EXACT LINE to tell Cloudflare to use your CSS
+  eleventyConfig.addPassthroughCopy("style.css");
+
   // This line defines the missing "limit" function for your grids
   eleventyConfig.addFilter("limit", function(array, limit) {
     return array.slice(0, limit);
